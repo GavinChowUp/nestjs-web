5090fbeef01b3a5eee6ae4479dc1a1d78e604ccd
GavinChow <gavin@GavinChowdeMacBook-Pro.local>
2020-01-17 09:34:15 +0800

    feature/crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {HeroesModule} from './module/heroes/heroes.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {PhotoModule} from './module/photo/photo.module';
+import {LoggingInterceptor} from './common/intercepter/LoggingInterceptor';
+import {APP_INTERCEPTOR} from '@nestjs/core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot(),
+        HeroesModule,
+        PhotoModule,
+    ],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: LoggingInterceptor,
+        },
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {LoggingInterceptor} from './common/intercepter/LoggingInterceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('mysql-orm example')
+        .setDescription('The example API description')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+    console.log('http://localhost:3000/api-docs');
+
+    app.useGlobalInterceptors(new LoggingInterceptor());
+
+    await app.listen(3000);
 }
+
 bootstrap();
