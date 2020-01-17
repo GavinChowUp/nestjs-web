f3969ccd443b567b426cc661fe62ebe43c5e16b9
GavinChow <gavin@GavinChowdeMacBook-Pro.local>
2020-01-17 13:59:59 +0800

    micro-service

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {LoggingInterceptor} from './common/intercepter/LoggingInterceptor';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: ['log', 'debug'],
+    });
 
     const options = new DocumentBuilder()
         .setTitle('mysql-orm example')
